b814fe03f57e1e31b7058dc10a33e2f2fdeda16c
0nefinity <130517268+0nefinity@users.noreply.github.com>
2024-12-14 00:10:45 +0100

    update generatejson.py

--- generatejson.py
+++ generatejson.py
@@ -1,8 +1,20 @@
 import os
 import json
 
-html_files = [f for f in os.listdir('.') if f.endswith('.html')]
+# Rekursive Suche nach .html-Dateien
+def find_html_files(directory):
+    html_files = []
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.html'):
+                # Speichere den relativen Pfad
+                html_files.append(os.path.relpath(os.path.join(root, file)))
+    return html_files
 
+# Dateien finden
+html_files = find_html_files('.')
+
+# Datei-Liste als JSON speichern
 with open('file-list.json', 'w') as file:
     json.dump(html_files, file, indent=4)
 
